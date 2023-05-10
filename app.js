d79dac40b53b2c72d40082f7df4f81b4ea085327
Naresh Tak <taknaresh515@gmail.com>
2023-05-10 17:06:18 +0530

    change some js

--- app.js
+++ app.js
@@ -2,14 +2,6 @@ $(document).ready(function () {
     $('.video-gallery').magnificPopup({
         delegate: 'a',
         type: 'iframe',
-        iframe: {
-            patterns: {
-                mp4: {
-                    index: '.mp4',
-                    src: '%href%'
-                }
-            }
-        },
         gallery: {
             enabled: true
         }
