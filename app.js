811d84b1b6eb7d88237e3f8c36fc914aa9d4ec48
Naresh Tak <taknaresh515@gmail.com>
2023-05-10 16:55:25 +0530

    change video formet

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ $(document).ready(function () {
     $('.video-gallery').magnificPopup({
         delegate: 'a',
         type: 'iframe',
+        iframe: {
+            patterns: {
+                mp4: {
+                    index: '.mp4',
+                    src: '%src%'
+                }
+            }
+        },
         gallery: {
             enabled: true
         }
